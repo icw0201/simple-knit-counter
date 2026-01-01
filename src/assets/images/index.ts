9e80b6c5ff694dc620e78c59769e287a8415e234
icw0201 <icw0207@naver.com>
2026-01-01 19:36:49 +0900

    refactor: 이미지 에셋 폴더 경로 정리

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,12 +1,12 @@
 export const activateIcons = {
-  inactive: require('./activate_inactive.png'),
-  active: require('./activate_active.png'),
-  auto: require('./activate_auto.png'),
+  inactive: require('./activate/activate_inactive.png'),
+  active: require('./activate/activate_active.png'),
+  auto: require('./activate/activate_auto.png'),
 };
 
 export const Images = {
-  way_front: require('./way_front.png'),
-  way_back: require('./way_back.png'),
-  complete_nomal: require('./complete_nomal.png'),
-  complete_reverse: require('./complete_reverse.png'),
+  way_front: require('./way/way_front.png'),
+  way_back: require('./way/way_back.png'),
+  complete_nomal: require('./complete/complete_nomal.png'),
+  complete_reverse: require('./complete/complete_reverse.png'),
 };
